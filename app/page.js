a7962f8d6234fe4dba136ddc2c3669ce5fb8130e
judgejumbo <jimbokennedy2018@gmail.com>
2024-12-15 12:35:13 +1000

    5 buttons

--- app/page.js
+++ app/page.js
@@ -4,6 +4,10 @@ export default function Home() {
   return (
     <main>
       <button className="btn btn-active btn-primary"> Get Started</button>
+      <button className="btn btn-outline">Default</button>
+      <button className="btn btn-outline btn-primary">Primary</button>
+      <button className="btn btn-outline btn-secondary">Secondary</button>
+      <button className="btn btn-outline btn-accent">Accent</button>
     </main>
   );
 }
