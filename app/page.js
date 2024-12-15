b35091487c0556c4546d4869205567024cb291f5
judgejumbo <jimbokennedy2018@gmail.com>
2024-12-15 15:29:47 +1000

    Deleted 1 Button
    I deletted one button on the second set

--- app/page.js
+++ app/page.js
@@ -14,7 +14,6 @@ export default function Home() {
         <button className="btn btn-active btn-primary"> Get Started</button>
         <button className="btn btn-outline">Default</button>
         <button className="btn btn-outline btn-primary">Primary</button>
-        <button className="btn btn-outline btn-secondary">Secondary</button>
         <button className="btn btn-outline btn-accent">Accent</button>
       </div>
     </main>
