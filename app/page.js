7793dfbb67cd2d1c47bb285fe716f4311a6113f4
judgejumbo <jimbokennedy2018@gmail.com>
2024-12-15 12:39:09 +1000

    2 rows

--- app/page.js
+++ app/page.js
@@ -3,11 +3,20 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main>
-      <button className="btn btn-active btn-primary"> Get Started</button>
-      <button className="btn btn-outline">Default</button>
-      <button className="btn btn-outline btn-primary">Primary</button>
-      <button className="btn btn-outline btn-secondary">Secondary</button>
-      <button className="btn btn-outline btn-accent">Accent</button>
+      <div className=" bg-slate-100 ">
+        <button className="btn btn-active btn-primary"> Get Started</button>
+        <button className="btn btn-outline">Default</button>
+        <button className="btn btn-outline btn-primary">Primary</button>
+        <button className="btn btn-outline btn-secondary">Secondary</button>
+        <button className="btn btn-outline btn-accent">Accent</button>
+      </div>
+      <div className=" bg-slate-500 ">
+        <button className="btn btn-active btn-primary"> Get Started</button>
+        <button className="btn btn-outline">Default</button>
+        <button className="btn btn-outline btn-primary">Primary</button>
+        <button className="btn btn-outline btn-secondary">Secondary</button>
+        <button className="btn btn-outline btn-accent">Accent</button>
+      </div>
     </main>
   );
 }
